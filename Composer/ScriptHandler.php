cbcf56aebde0e89e0c5d67863587e300f8b2fdeb
Joe Luis Borrero Senciales <jlborrero@gmail.com>
2013-09-05 14:28:30 -0400

    Actualizando

---
+++ Composer/ScriptHandler.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Script for composer, to symlink bootstrap lib into Bundle
+ *
+ * Maybe nice to convert this to a command and then reuse command in here.
+ */
+namespace Jlbs\AdminBundle\Command\Composer;
+
+use Composer\Script\Event;
+use Mopa\Bridge\Composer\Util\ComposerPathFinder;
+use Mopa\Bundle\BootstrapBundle\Command\BootstrapSymlinkLessCommand;
+use Mopa\Bundle\BootstrapBundle\Command\BootstrapSymlinkSassCommand;
+
+class ScriptHandler
+{
+
+    public static function postInstallSymlinkTwitterBootstrap(Event $event)
+    {
+        $IO = $event->getIO();
+        $composer = $event->getComposer();
+        $cmanager = new ComposerPathFinder($composer);
+        $options = array(
+            'targetSuffix' => DIRECTORY_SEPARATOR . "Resources" . DIRECTORY_SEPARATOR . "bootstrap",
+            'sourcePrefix' => '..' . DIRECTORY_SEPARATOR
+        );
+        list($symlinkTarget, $symlinkName) = $cmanager->getSymlinkFromComposer(
+            BootstrapSymlinkLessCommand::$mopaBootstrapBundleName,
+            BootstrapSymlinkLessCommand::$twitterBootstrapName,
+            $options
+        );
+
+        $IO->write("Checking Symlink", FALSE);
+        if (false === BootstrapSymlinkLessCommand::checkSymlink($symlinkTarget, $symlinkName, true)) {
+            $IO->write("Creating Symlink: " . $symlinkName, FALSE);
+            BootstrapSymlinkLessCommand::createSymlink($symlinkTarget, $symlinkName);
+        }
+        $IO->write(" ... <info>OK</info>");
+    }
+}
